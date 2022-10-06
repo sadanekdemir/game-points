397e661581340a538883e6bb4b30492daee6a1b1
Sadan Ekdemir <sadan@vev.design>
2022-10-07 00:30:10 +0200

    adding some styling with css

--- game-points/src/App.tsx
+++ game-points/src/App.tsx
@@ -31,7 +31,6 @@ interface ScoreItem {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
   const [score, setScore] = useState(0);
   const [totalBonus, setTotalBonus] = useState(0);
 
@@ -48,8 +47,6 @@ function App() {
   },[clickedItems])
 
   const handleClickedItem = useCallback(() => {
-    // this is the shit
-
     const grouped: GroupedItem = clickedItems.reduce((items: GroupedItem, item: GameItem) => {
       const temp = item['label'];
 
@@ -79,63 +76,83 @@ function App() {
 
   }, [clickedItems])
 
-
-  const GenericPanel = ({ title, children, width} : { title: string | React.ReactNode, children: React.ReactNode, width?: number }) => {
-    return (
-      <div className={styles.panel}>
-        <div className={styles.title}>
-          {title}
-        </div>
-        {children}
-      </div>
-    )
+  const handleResetGame = () => {
+    setGroupedItems({});
+    setClickedItems([]);
+    setTotalBonus(0);
+    setScore(0);
   }
 
-  const GameLeftPanel = ({ title, onClick, style }:{ title: React.ReactNode, onClick: (item: GameItem) => void, style?: React.CSSProperties; }) => {
-    return (
-      <GenericPanel title={title} width={500}>
-        <div className={styles.wrapper}>
+  console.log('i am rendered')
+
+  return (
+    <div className={styles.gameApp}>
+      <GameLeftPanel title="Kahoot! POINTS" onClick={(item) => handleClick(item)} />
+      <GameRightPanel title="PLAYER ITEMS" onClick={handleResetGame} groupedItems={groupedItems} totalBonus={totalBonus} totalScore={score} />
+    </div>
+  )
+}
+
+const GenericPanel = ({ title, children, width} : { title: string | React.ReactNode, children: React.ReactNode, width?: number }) => {
+  return (
+    <div className={styles.panel}>
+      <div className={styles.title}>
+        {title}
+      </div>
+      {children}
+    </div>
+  )
+}
+
+const GameLeftPanel = ({ title, onClick, style }:{ title: React.ReactNode, onClick: (item: GameItem) => void, style?: React.CSSProperties; }) => {
+  return (
+    <GenericPanel title={title} width={600}>
+      <div className={styles.leftPanel}>
+        <h4>ITEMS</h4>
+        <div className={styles.gameWrapper}>
           {items.map(item => (
             <div key={item.label} className={styles.item} onClick={() => onClick(item)}>{item.label}</div>
           ))}
         </div>
-      </GenericPanel>
-    )
-  }
+      </div>
+    </GenericPanel>
+  )
+}
 
-  const calculateScorePerItem = (item: GameItem, quantity: number) => {
-    const { unitPoints, bonus} = item;
-    const length = quantity;
-    const isBonusCondition = bonus && length >= bonus.collect && unitPoints * length < bonus.yield;
-      
-    // check if there's bonus
-    const score = isBonusCondition ? bonus.yield + (length - bonus.collect) * unitPoints : unitPoints * length;
+const calculateScorePerItem = (item: GameItem, quantity: number) => {
+  const { unitPoints, bonus} = item;
+  const length = quantity;
+  const isBonusCondition = bonus && length >= bonus.collect && unitPoints * length < bonus.yield;
+    
+  // check if there's bonus
+  const score = isBonusCondition ? bonus.yield + (length - bonus.collect) * unitPoints : unitPoints * length;
 
-    return score;
-  }
+  return score;
+}
 
-  const GameRightPanel = ({title, groupedItems, onClick, style}: {title: string, onClick: () => void, groupedItems: GroupedItem, style?: React.CSSProperties;}) => {
+const GameRightPanel = ({title, groupedItems, totalBonus, totalScore, onClick, style}: {title: string, totalBonus: number, totalScore: number, onClick: () => void, groupedItems: GroupedItem, style?: React.CSSProperties;}) => {
 
-    console.log('im hit')
+  console.log('im hit')
 
-    const scoreItems: ScoreItem[] = useMemo(() => {
-      return Object.keys(groupedItems).map(label => ( {
-        label: label,
-        quantity: groupedItems[label].length,
-        score: calculateScorePerItem(groupedItems[label][0], groupedItems[label].length),
-      }))
-    }, [groupedItems]) 
+  const scoreItems: ScoreItem[] = useMemo(() => {
+    return Object.keys(groupedItems).map(label => ( {
+      label: label,
+      quantity: groupedItems[label].length,
+      score: calculateScorePerItem(groupedItems[label][0], groupedItems[label].length),
+    }))
+  }, [groupedItems]) 
 
-    return (
-      <GenericPanel title={title}>
-        {/* this will be in a context later and will listen to the bonuses n stuff, for now all in the same component */}
+  return (
+    <GenericPanel title={title}>
+      {/* this will be in a context later and will listen to the bonuses n stuff, for now all in the same component */}
+      <div className={styles.rightPanel}>
         <div className={styles.scoreWrapper}>
           <div className={styles.scoreItemsHolder}>
             <div>Item</div>
             <div>Quantity</div>
             <div>Score</div>
           </div>
-          {clickedItems.length > 0 && 
+          {scoreItems.length > 0 && 
             scoreItems.map(scoreItem => (
               <div key={scoreItem.label} className={styles.scoreItemsHolder}>
                 <div className={styles.scoreItemLabel}>{scoreItem.label}</div>
@@ -144,21 +161,21 @@ function App() {
               </div>
             ))
           }
-          <div className={styles.scoreItem}></div>
         </div>
-        <div>Bonus n shit: {totalBonus}</div>
-        <div>Score n shit: {score}</div>
-      </GenericPanel>
-    )
-  }
+        <div>
+          <div className={styles.bonuses}>Bonuses: {totalBonus}</div>
+          <div className={styles.totalScore}>
+            <div>
+              Total <h3> {totalScore} </h3>
+            </div>
 
-  console.log('i am rendered')
-
-  return (
-    <div className={styles.gameApp}>
-      <GameLeftPanel title="Kahoot! POINTS" onClick={(item) => handleClick(item)} />
-      <GameRightPanel title="PLAYER ITEMS" onClick={() => {}} groupedItems={groupedItems} />
-    </div>
+            <button onClick={onClick}>
+              New game
+            </button>
+          </div>
+        </div>
+      </div>
+    </GenericPanel>
   )
 }
 
